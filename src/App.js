b75727d1ae7436b4fe9b59d4168e6c711be1ac50
chdivyavani15 <chdivyavani15@gmail.com>
2021-12-30 10:29:54 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
+import ListStudentComponent from './components/ListStudentComponent';
+import HeaderComponent from './components/HeaderComponent';
+import FooterComponent from './components/FooterComponent';
+import CreateStudentComponent from './components/CreateStudentComponent';
+import ViewStudentComponent from './components/ViewStudentComponent';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+        <Router>
+              <HeaderComponent />
+                <div className="container">
+                    <Switch>
+                          <Route path = "/" exact component = {ListStudentComponent}></Route>
+                          <Route path = "/student" component = {ListStudentComponent}></Route>
+                          <Route path = "/add-student/:sid" component = {CreateStudentComponent}></Route>
+                          <Route path = "/view-student/:sid" component = {ViewStudentComponent}></Route>
+                          {/* <Route path = "/update-student/:sid" component = {UpdateStudentComponent}></Route> */}
+                    </Switch>
+                </div>
+              <FooterComponent />
+        </Router>
     </div>
+   
   );
 }
-
 export default App;
